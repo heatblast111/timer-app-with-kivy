b19914495c0923702e8c77d8194d25f6230da411
Heatblast <rajachandra64234@gmail.com>
2025-02-08 21:48:25 +0530

    boilerplate setup

--- timer_app.py
+++ timer_app.py
@@ -1,1 +1,12 @@
 import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class OutWindow(Widget):
+    pass
+class TimerApp(App):
+    def build(self):
+        return OutWindow()
+
+if __name__ == '__main__':
+    TimerApp().run()
